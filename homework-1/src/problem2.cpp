613044037b3e2d35b2a4e94d6c5fc2df23cb8c39
i-am-bad-gay <165265930+i-am-bad-gay@users.noreply.github.com>
2025-10-13 20:51:28 +0800

    Update problem2.cpp

--- homework-1/src/problem2.cpp
+++ homework-1/src/problem2.cpp
@@ -1,34 +1,43 @@
 #include <iostream>
 using namespace std;
 
-void PowerSet(string a,int ii,int end,string c)
+void powerSet(string s, int i, string c)
 {
-    if(ii!=end)
-    {
-        c+=a[ii];
-        cout<< "(" << c << ")";
-        PowerSet(a,ii+1,end,c+",");
-    }
     
+    if (i == s.size())  //走到底就執行 沒到底就繼續往下繼續走
+    {
+        cout << "(";
+        for (int i = 0; i < c.size(); i++)
+        {
+            cout << c[i];
+            if (i != c.size() - 1)
+                cout << ",";
+        }
+        cout << "),";
+        return;
+    }
+
+    powerSet(s, i + 1, c);  //一直往下走 順便讓 i 記錄往下走了幾次 
+
+    c+= s[i];  //走到底之後 回來 選擇一個字元
+    powerSet(s, i+1, c); //帶著字元走到底
 }
 
 
 int main()
 {
+    string input;
+    getline(cin, input); 
     string a;
-    char b[1001]={'\0'};
-    getline(cin,a);
-    for(int i=0;i<a.size();i+=2)b[i/2]=a[i];  //取出字母
-    int n = 0;
-    while (b[n] != '\0')n++; // 計算元素個數
 
-    cout << "powerSet(S) = {( ),";
-    string B(b);
-    for(int i=0;i<n;i++)
-    {
-        PowerSet(B,i,n,"");
+    for (char c : input) {  
+        if (c != ',') {      
+            a += c;
+        }
     }
+
+    cout<<"powerSet={ ";
+    powerSet(a,0,"");
     
-   
     cout << " }";
 }
